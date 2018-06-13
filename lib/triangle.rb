f77a4d51dff29e6e547438b293ec5e6b0c0fe8c3
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-13 23:43:00 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,3 +17,10 @@ class Triangle
     end
   end
 end
+
+    class TriangleError< StandardError 
+      def message
+      puts The sum of the lengths of any two sides of a triangle always exceeds the length of the third side
+    end 
+  end
+  
